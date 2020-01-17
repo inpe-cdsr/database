c11216ed77efdb2267bc504c694feb0511e19ad8
Rodrigo Mariano <rodmariano13@gmail.com>
2020-01-17 15:54:08 -0300

    View was created

---
+++ scripts/views.sql
@@ -0,0 +1,9 @@
+CREATE OR REPLACE VIEW `collection` AS
+SELECT d.Name as id, d.Description as description,
+        MIN(s.Date) as start_date, MAX(s.Date) as end_date,
+        MIN(BL_Latitude) as min_y, MIN(BL_Latitude) as min_x,
+        MAX(TR_Latitude) as max_x, MAX(TR_Longitude) as max_y
+FROM Scene s, Product p, Dataset d
+WHERE s.sceneId = p.sceneId AND d.name = p.Dataset
+GROUP BY p.Dataset
+ORDER BY p.Dataset;
