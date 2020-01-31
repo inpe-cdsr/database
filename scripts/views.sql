460bcff0cbff59ac6fb9e31791a4b7cab9814059
Rodrigo Mariano <rodmariano13@gmail.com>
2020-01-31 12:23:13 -0300

    Item view was created

--- scripts/views.sql
+++ scripts/views.sql
@@ -19,3 +19,17 @@ FROM Scene s, Product p, Dataset d
 WHERE s.sceneId = p.sceneId AND d.name = p.Dataset
 GROUP BY p.Dataset
 ORDER BY p.Dataset;
+
+
+CREATE OR REPLACE VIEW `item` AS
+SELECT a.SceneId as id,
+        b.Dataset as collection,
+        Date as date, Path as path, Row as row, Satellite as satellite, Sensor as sensor, CloudCoverQ1 as cloud_cover,
+        q.QLfilename as thumbnail,
+        GROUP_CONCAT(CONCAT('{"band": "', b.band, '", "filename": "', b.filename,'"}')) as assets,
+        TL_Longitude, TL_Latitude, BL_Longitude, BL_Latitude,
+        BR_Longitude, BR_Latitude, TR_Longitude, TR_Latitude
+FROM Scene a, Product b, Dataset c, Qlook q
+WHERE a.SceneId = b.SceneId AND b.Dataset = c.Name AND b.SceneId = q.SceneId
+GROUP BY a.SceneId
+ORDER BY a.Date DESC, a.SceneId ASC;
