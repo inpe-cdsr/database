9c22ba82a7cdcade377c8daf731b396fd0fe1f77
Rodrigo Mariano <rodmariano13@gmail.com>
2020-02-17 16:33:38 -0300

    Views and diagram were updated

--- scripts/views.sql
+++ scripts/views.sql
@@ -11,10 +11,14 @@ USE `catalogo`;
 --
 
 CREATE OR REPLACE VIEW `stac_collection` AS
-SELECT d.Name as id, d.Description as description,
-        MIN(s.Date) as start_date, MAX(s.Date) as end_date,
-        MIN(BL_Latitude) as min_y, MIN(BL_Latitude) as min_x,
-        MAX(TR_Latitude) as max_x, MAX(TR_Longitude) as max_y
+SELECT d.Name id,
+        d.Description description,
+        MIN(s.Date) start_date,
+        MAX(s.Date) end_date,
+        MIN(BL_Latitude) min_y,
+        MIN(BL_Latitude) min_x,
+        MAX(TR_Latitude) max_x,
+        MAX(TR_Longitude) max_y
 FROM Scene s, Product p, Dataset d
 WHERE s.sceneId = p.sceneId AND d.name = p.Dataset
 GROUP BY p.Dataset
@@ -22,13 +26,28 @@ ORDER BY p.Dataset;
 
 
 CREATE OR REPLACE VIEW `stac_item` AS
-SELECT s.SceneId as id,
-        p.Dataset as collection,
-        Date as date, Path as path, Row as row, Satellite as satellite, Sensor as sensor, CloudCoverQ1 as cloud_cover,
-        q.QLfilename as thumbnail,
-        CONCAT('[', GROUP_CONCAT(CONCAT('{"band": "', p.band, '", "href": "', p.filename,'"}')), ']') as assets,
-        TL_Longitude, TL_Latitude, BL_Longitude, BL_Latitude,
-        BR_Longitude, BR_Latitude, TR_Longitude, TR_Latitude
+SELECT s.SceneId id,
+        p.Dataset collection,
+        s.Date date,
+        s.CenterTime center_time,
+        s.Path path,
+        s.Row row,
+        s.Satellite satellite,
+        s.Sensor sensor,
+        s.CloudCover cloud_cover,
+        s.SyncLoss sync_loss,
+        q.QLfilename thumbnail,
+        CONCAT('[',
+                GROUP_CONCAT(CONCAT('{"band": "', p.band, '", "href": "', p.filename,'"}')),
+        ']') assets,
+        TL_Longitude tl_longitude,
+        TL_Latitude tl_latitude,
+        BL_Longitude bl_longitude,
+        BL_Latitude bl_latitude,
+        BR_Longitude br_longitude,
+        BR_Latitude br_latitude,
+        TR_Longitude tr_longitude,
+        TR_Latitude tr_latitude
 FROM Scene s, Product p, Dataset d, Qlook q
 WHERE s.SceneId = p.SceneId AND p.Dataset = d.Name AND p.SceneId = q.SceneId
 GROUP BY s.SceneId
