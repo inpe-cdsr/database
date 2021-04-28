162513b1ef03acbbe622031ae8e4dc9473cef8c8
Rodrigo Mariano <rodmariano13@gmail.com>
2021-04-28 12:22:19 -0300

    chore: remove view

--- scripts/postgresql/02_views.sql
+++
@@ -1,7 +0,0 @@
-
-CREATE OR REPLACE VIEW bdc.stac_item AS
-SELECT CONCAT(c.name, '-', c.version) collection,
-        i.*
-FROM bdc.items i, bdc.collections c
-WHERE i.collection_id = c.id
-ORDER BY c.name, i.start_date DESC;
