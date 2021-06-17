df0508678e4ea869054ba0f2a106eca7cc4eb9d7
Rodrigo Mariano <rodmariano13@gmail.com>
2021-06-17 10:57:07 -0300

    chore: add procedures to update Dataset records

--- scripts/03_procedures.sql
+++ scripts/03_procedures.sql
@@ -82,3 +82,76 @@ BEGIN
         WHERE sceneId IS NULL OR CHAR_LENGTH(sceneId) <= 8;
 END |
 DELIMITER ;
+
+
+--------------------------------------------------
+-- update Dataset table records
+--------------------------------------------------
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS `update_collection_fields` $$
+CREATE PROCEDURE update_collection_fields(collection_name TEXT)
+BEGIN
+    -- this script looks for the min/max values of a collection
+    -- and update its record with them
+
+    UPDATE Dataset d
+        INNER JOIN (
+            SELECT collection,
+                MIN(datetime) start_date,
+                MAX(datetime) end_date,
+                MIN(bl_longitude) min_x,
+                MIN(bl_latitude) min_y,
+                MAX(tr_longitude) max_x,
+                MAX(tr_latitude) max_y
+            FROM stac_item
+            WHERE collection = collection_name
+            GROUP BY collection
+        ) si ON d.Name = si.collection
+        SET d.start_date = si.start_date,
+            d.end_date = si.end_date,
+            d.min_y = si.min_y,
+            d.min_x = si.min_x,
+            d.max_y = si.max_y,
+            d.max_x = si.max_x
+        WHERE Name = collection_name;
+END $$
+DELIMITER ;
+
+-- example
+-- CALL update_collection_fields('CBERS4A_MUX_L2_DN');
+-- CALL update_collection_fields('CBERS4A_MUX_L4_DN');
+-- CALL update_collection_fields('CBERS4A_WFI_L2_DN');
+-- CALL update_collection_fields('CBERS4A_WFI_L4_DN');
+-- CALL update_collection_fields('CBERS4A_WPM_L2B_DN');
+-- CALL update_collection_fields('CBERS4A_WPM_L2_DN');
+-- CALL update_collection_fields('CBERS4A_WPM_L4_DN');
+
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS `update_dataset_records` $$
+CREATE PROCEDURE update_dataset_records()
+BEGIN
+    -- this procedure updates each row of Dataset table
+    -- based on the available items
+
+    DECLARE size_dataset INT DEFAULT 0;
+    DECLARE i INT DEFAULT 0;
+    DECLARE collection_name VARCHAR(20);
+
+    SELECT COUNT(*) FROM Dataset INTO size_dataset;
+
+    -- iterate over Dataset rows and call procedure to each collection
+    SET i=0;
+    WHILE i<size_dataset DO
+        -- get collection name on the position 'i'
+        SELECT Name FROM Dataset ORDER BY Name LIMIT i,1 INTO collection_name;
+        -- call procedure passing the collection name
+        CALL update_collection_fields(collection_name);
+        SET i=i+1;
+    END WHILE;
+END $$
+DELIMITER ;
+
+-- example
+-- CALL update_dataset_records();
